96e076e4e70e5752536e7b6767f4cdd9d19d38c6
dlevene <10554724+dlevene1@users.noreply.github.com>
2022-04-13 11:10:10 +0930

    updates

--- drip_compound.py
+++ drip_compound.py
@@ -3,22 +3,26 @@ import os
 import logging
 import sys
 import time
-#from settings import *
+from pushover import Client
 from utils import eth2wei, wei2eth, pancakeswap_api_get_price, read_json_file, to_checksum
-
 
 DRIP_TOKEN_ADDRESS = "0xFFE811714ab35360b67eE195acE7C10D93f89D8C"
 DRIP_FAUCET_ABI_FILE = "./abis/Faucet.json"
 VERSION = '0.2'
 
 class DripCompundClass:
-    def __init__(self, private_key, txn_timeout=120, gas_price=5, rpc_host="https://bsc-dataseed.binance.org:443",min_balance=0.015,rounding=3):
+    def __init__(self, private_key, pushover_api_key, pushover_user_key, txn_timeout=120, gas_price=5, rpc_host="https://bsc-dataseed.binance.org:443",min_balance=0.015, rounding=3, **kwargs):
         self.private_key = private_key
         self.rounding = rounding
         self.min_balance = min_balance
         self.txn_timeout = txn_timeout
         self.gas_price = gas_price
         self.rpc_host = rpc_host
+        self.pushover_api_key = pushover_api_key
+        self.pushover_user_key = pushover_user_key
+
+        self.client = Client(self.pushover_user_key, api_token=self.pushover_api_key)
+
         # Initialize web3, and load the smart contract objects.
         self.w3 = Web3(Web3.HTTPProvider(self.rpc_host))
         self.account = self.w3.eth.account.privateKeyToAccount(self.private_key)
@@ -62,9 +66,14 @@ class DripCompundClass:
         signed_tx = self.w3.eth.account.sign_transaction(tx, self.private_key)
         txn = self.w3.eth.send_raw_transaction(signed_tx.rawTransaction)
         logging.info("Transaction: %s" % (self.w3.toHex(txn)))
-        time.sleep(5)
+        time.sleep(10)
         self.getDripBalance()
         logging.info("Updated Drip balance is: %s" % self.DripBalance)
+        self.sendMessage("Drip Compounding Complete","Updated Balance %s - tx %s" % (self.DripBalance,self.w3.toHex(txn)))
+
+
+    def sendMessage(self, title_txt, body):
+        self.client.send_message(body, title=title_txt)
 
 
 def main():
@@ -75,21 +84,24 @@ def main():
     logging.info('----------------')
     #logging.info('Strategy: %s.' % ACTION_LIST)
 
-    MANDATORY_ENV_VARS = ["PRIVATE_KEY"]
+    MANDATORY_ENV_VARS = ["PRIVATE_KEY","PUSHOVER_API_KEY","PUSHOVER_USER_KEY"]
 
     for var in MANDATORY_ENV_VARS:
         if var not in os.environ:
             raise EnvironmentError("Failed because {} is not set.".format(var))
 
     PRIVATE_KEY = os.environ['PRIVATE_KEY']
+    PUSHOVER_API_KEY = os.environ['PUSHOVER_API_KEY']
+    PUSHOVER_USER_KEY = os.environ['PUSHOVER_USER_KEY']
         
-    drip1wallet = DripCompundClass(private_key=PRIVATE_KEY)
+    dripwallet = DripCompundClass(private_key=PRIVATE_KEY,pushover_api_key=PUSHOVER_API_KEY,pushover_user_key=PUSHOVER_USER_KEY)
 
-    logging.info("Current Balance %s" % drip1wallet.DripBalance)
-    logging.info("Available to compund %s" % drip1wallet.claimsAvailable)
+    logging.info("Current Balance %s" % dripwallet.DripBalance)
+    logging.info("Available to compound %s" % dripwallet.claimsAvailable)
+    dripwallet.sendMessage("Drip Compounding","Current Balance %s - Compound %s" % (dripwallet.DripBalance,dripwallet.claimsAvailable))
 
     # Actually do the compound step
-    #drip1wallet.compundDrip()
+    dripwallet.compundDrip()
 
 
 main()
